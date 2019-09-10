ba500b419b97129a1cf410a0f109f059bbf1614d
Miguel Serrano <mserranocaldeiro@mserrano-ltm3.internal.salesforce.com>
2019-09-10 12:43:28 +0200

    segundo commit

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
+
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(function(req, res, next) {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization');
-      res.setHeader('Access-Control-Allow-Methods', '*');
-      res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
-      res.setHeader('Access-Control-Max-Age', '1000');
-      next();
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
+    res.setHeader('Access-Control-Max-Age', '1000');
+    next();
   })
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
